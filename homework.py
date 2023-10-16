1752add5a982d3c06766b4214015c8c7709fab2b
Кирилл Завадский <zavad93@mail.ru>
2023-10-16 20:15:16 +0300

    Fixed file homework.py.

--- homework.py
+++ homework.py
@@ -1,13 +1,13 @@
 import logging
 import os
+import sys
+import time
+from http import HTTPStatus
+from logging import Formatter, StreamHandler
 
 import requests
-import sys
 import telegram
-import time
-
 from dotenv import load_dotenv
-from logging import Formatter, StreamHandler
 
 load_dotenv()
 
@@ -25,93 +25,107 @@ HOMEWORK_VERDICTS = {
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-
-logging.basicConfig(
-    format='[%(asctime)s : %(levelname)s] - %(message)s',
-    level=logging.DEBUG,
-    filename='main.log'
-)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 handler = StreamHandler(stream=sys.stdout)
 handler.setFormatter(
-    Formatter(fmt='[%(asctime)s : %(levelname)s] - %(message)s')
+    Formatter(
+        fmt='[%(pathname)s : %(asctime)s : %(levelname)s] -'
+        '%(message)s, %(module)s'
+    )
 )
 logger.addHandler(handler)
 
 
+class EmptyResponseApi(Exception):
+    pass
+
+
+class UnexpectedStatusCode(Exception):
+    pass
+
+
+class NoVariableToken(Exception):
+    pass
+
+
 def check_tokens():
     """Проверяет доступность переменных окружения."""
-    if TELEGRAM_TOKEN is None:
-        logging.critical('No variable TELEGRAM_TOKEN.')
-        sys.exit()
-    elif PRACTICUM_TOKEN is None:
-        logging.critical('No variable PRACTICUM_TOKEN.')
-        sys.exit()
-    elif TELEGRAM_CHAT_ID is None:
-        logging.critical('No variable TELEGRAM_CHAT_ID.')
-        sys.exit()
+    tokens = {
+        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID
+    }
+    chek_tokens = []
+    for key, value in tokens.items():
+        if value is None:
+            chek_tokens.append(key)
+        if chek_tokens:
+            logger.critical(f'No variable: {chek_tokens}')
+            raise NoVariableToken(f'No variable: {chek_tokens}')
+        logger.debug('Chek tokens - OK')
 
 
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат."""
     try:
+        logging.debug('Start message sent')
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
         logging.debug('Message sent')
-    except Exception as error:
+    except telegram.error.TelegramError as error:
         logging.error(f'Crash when sending message to Telegram: {error}')
-        logger.error(f'Crash when sending message to Telegram: {error}')
 
 
 def get_api_answer(timestamp):
     """Делает запрос к эндпоинту API-сервиса."""
-    payload = {'from_date': timestamp}
+    payload = {
+        'url': ENDPOINT,
+        'headers': HEADERS,
+        'params': {'from_date': timestamp}
+    }
     try:
-        hw_status = requests.get(
-            ENDPOINT,
-            headers=HEADERS,
-            params=payload
+        logging.debug(
+            'Response start'
+            'url: {url},'
+            'headers: {headers},'
+            'params: {params}'.format(**payload)
         )
-        if hw_status.status_code != 200:
-            logger.error('ENDPOINT inaccessible')
-            logging.error('ENDPOINT inaccessible')
-            raise ConnectionError('ENDPOINT inaccessible')
+        hw_status = requests.get(**payload)
+        if hw_status.status_code != HTTPStatus.OK:
+            logging.error('Status code not 200.OK')
+            raise UnexpectedStatusCode('Status code not 200.OK')
         return hw_status.json()
     except requests.RequestException:
-        print('Message not sent')
+        logging.error('Response fail')
 
 
 def check_response(response):
     """Проверяет ответ API на соответствие документации."""
     if not isinstance(response, dict):
-        logger.error('Tyrpe of response not dict')
-        logging.error('Tyrpe of response not dict')
-        raise TypeError('Tyrpe of response not dict')
+        logging.error('Type of response not dict')
+        raise TypeError('Type of response not dict')
     if 'homeworks' not in response:
-        logger.error('No key "homeworks"')
         logging.error('No key "homeworks"')
-        raise ConnectionError('No key "homeworks"')
-    check_api = response['homeworks']
-    if not isinstance(check_api, list):
+        raise EmptyResponseApi('No key "homeworks"')
+    homeworks = response['homeworks']
+    if not isinstance(homeworks, list):
         logging.error('Error in request API')
-        logger.error('Error in request API')
         raise TypeError('Error in request API')
     logging.debug('Successful check API')
-    return check_api
+    return homeworks
 
 
 def parse_status(homework):
     """Извлекает из информации о конкретной домашней работе статус работы."""
     if 'homework_name' not in homework or 'status' not in homework:
-        logging.error('Missing expected keys in API')
         logger.error('Missing expected keys in API')
-        raise ConnectionError('Missing expected keys in API')
+        raise KeyError('Missing expected keys in API')
     status = homework.get('status')
     homework_name = homework.get('homework_name')
     if status not in HOMEWORK_VERDICTS:
         logging.error('Unexpected status of homework')
         logger.error('Unexpected status of homework')
-        raise ConnectionError('Unexpected status of homework')
+        raise ValueError('Unexpected status of homework')
     verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -121,14 +135,15 @@ def main():
     check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     send_message(bot, 'Work harder')
-    timestamp = int(time.time())
+    timestamp = 0
     last_message = ''
     while True:
         try:
             response = get_api_answer(timestamp)
-            homework = check_response(response)
-            if homework:
-                message = parse_status(homework[0])
+            timestamp = response.get('current_date')
+            homeworks = check_response(response)
+            if homeworks:
+                message = parse_status(homeworks[0])
             else:
                 message = 'Статус проверки не изменился.'
             if message != last_message:
@@ -136,13 +151,20 @@ def main():
                 last_message = message
             else:
                 logging.debug('No new statuses in response')
-                time.sleep(RETRY_PERIOD)
-
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
+            logging.error('Program fail')
+            if message != last_message:
+                message = f'Сбой в работе программы: {error}'
+                send_message(bot, message)
+                last_message = message
+        finally:
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='[%(asctime)s : %(levelname)s] - %(message)s',
+        level=logging.DEBUG,
+        filename='main.log'
+    )
     main()
