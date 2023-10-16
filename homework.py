8c7bee8ac5eb0840f8485ed0069041630b263a40
Кирилл Завадский <zavad93@mail.ru>
2023-10-16 20:21:42 +0300

    Fixed file homework.py. PEP8.

--- homework.py
+++ homework.py
@@ -38,14 +38,20 @@ logger.addHandler(handler)
 
 
 class EmptyResponseApi(Exception):
+    """Пустой ответ АПИ."""
+
     pass
 
 
 class UnexpectedStatusCode(Exception):
+    """Неожиданный статус-код."""
+
     pass
 
 
 class NoVariableToken(Exception):
+    """Отсутствуют переменные окружения."""
+
     pass
 
 
